c646dd843bc9b5d530929ae36c5a3d421504df0f
ramasedang <ramasedang@gmail.com>
2023-03-20 22:58:22 +0700

    fastify

--- gate-js/index.js
+++ gate-js/index.js
@@ -1,6 +1,6 @@
-import fastify from "fastify";
-import goQuery from "./db.js";
-import LRUCache from "lru-cache";
+import fastify from 'fastify';
+import goQuery from './db.js';
+import LRUCache from 'lru-cache';
 
 const app = fastify();
 
@@ -12,33 +12,54 @@ const cache = new LRUCache({
   maxAge: 1000 * 60 * 2,
 });
 
-// cache middleware
-
-const cacheMiddleware = async (request, reply, next) => {
-  const key = request.url;
-  const cached = cache.get(key);
-  if (cached) {
-    reply.send(cached);
-  } else {
-    next();
-  }
-};
-
-app.get("/", async (request, reply) => {
-  return { hello: "world" };
+app.get('/', async (request, reply) => {
+  return { hello: 'world' };
 });
 
-// test db connection and cache
-
-app.get("/test", async (request, reply) => {
+app.get('/test', async (request, reply) => {
   try {
     if (cache.has(request.url)) {
-      console.log("cache hit");
+      console.log('cache hit');
       return cache.get(request.url);
     } else {
-      const result = await goQuery("SELECT * FROM kartu_akses");
+      const result = await goQuery('SELECT * FROM kartu_akses');
       cache.set(request.url, result);
-      console.log("cache miss");
+      console.log('cache miss');
+      return result;
+    }
+  } catch (error) {
+    throw error;
+  }
+});
+
+// post idkartu , idgate
+app.post('/gate', async (request, reply) => {
+  try {
+    const { idkartu, idgate } = request.body;
+    const result = await goQuery(
+      `SELECT * FROM kartu_akses WHERE idkartu = ${idkartu} AND idgate = ${idgate}`
+    );
+    return result;
+  } catch (error) {
+    throw error;
+  }
+});
+
+app.post('/cari_pengguna', async (request, reply) => {
+  try {
+    const { id_pengguna } = request.body;
+    let result;
+    //cache
+    if (cache.has(request.url)) {
+      console.log('cache hit');
+      result = cache.get(request.url);
+      return result;
+    } else {
+      result = await goQuery(
+        `SELECT * FROM pengguna WHERE id_pengguna = '${id_pengguna}'`
+      );
+      cache.set(request.url, result);
+      console.log('cache miss');
       return result;
     }
   } catch (error) {
@@ -50,7 +71,7 @@ try {
   await app.listen({
     port: 3000,
   });
-  console.log("Server listening on port 3000");
+  console.log('Server listening on port 3000');
 } catch (error) {
   app.log.error(error);
   process.exit(1);
